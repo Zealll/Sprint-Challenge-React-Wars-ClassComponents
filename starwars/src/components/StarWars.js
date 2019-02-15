77ba01d4137c650ae4afce47bdbd57e58b2ff98d
unknown <eriznis@gmail.com>
2019-02-15 12:32:12 -0500

    elements chosen and displayed

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './StarWars.css'
 
 const StarWars = props =>{
     return (
         <div className='starWars'>
-            {props.charList.birth_year}
+        <div className='elements'>
+                <p><strong>Name:</strong> {props.charList.name}</p>
+            </div>
+            <div className='elements'>
+                <p><strong>DOB:</strong> {props.charList.birth_year}</p>
+            </div>
+            <div className='elements'>
+                <p><strong>Sex:</strong> {props.charList.gender}</p>
+            </div>
+            <div className='elements'>
+                <p><strong>Eye Color:</strong> {props.charList.eye_color}</p>
+            </div>
+            <div className='elements'>
+                <p><strong>Height:</strong> {props.charList.height}</p>
+            </div>
         </div>
     )
 }
